66ab8cd879c867119e70a117585c7cfad36bcf11
AustinRado <radoaussy@gmail.com>
2024-01-16 12:38:18 -0800

    initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers.
+"""
+
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Query the number of subscribers for a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyUserAgent"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code != 200:
+            return 0
+
+        data = response.json().get("data", {})
+        subscribers = data.get("subscribers", 0)
+        return subscribers
+
+    except requests.exceptions.RequestException:
+        return 0
