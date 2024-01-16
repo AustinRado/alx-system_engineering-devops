7b2c30f5457f2165f16508e0d23689954a25c9b7
AustinRado <radoaussy@gmail.com>
2024-01-16 12:51:48 -0800

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
 
 
-def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
-                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
-    with requests.session() as client:
-        info = client.get(url, headers=headers, allow_redirects=False).json()
-        try:
-            return info.get('data', {}).get('subscribers', 0)
-        except Exception:
-            return 0
+def top_ten(subreddit):
+    """Print titles of the top ten hot posts for a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "MyUserAgent"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code != 200:
+            print(None)
+            return
+
+        data = response.json().get("data", {})
+        children = data.get("children", [])
+        top_posts = [(child["data"]["name"], child["data"]["title"],
+                      child["data"]["ups"]) for child in children][:10]
+
+        for post in top_posts:
+            print(post[1])
+
+    except requests.exceptions.RequestException:
+        print(None)
