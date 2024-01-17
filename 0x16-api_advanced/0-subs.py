f59971ca332a1b3dfc742eac0a53969f6cd1b5bc
AustinRado <radoaussy@gmail.com>
2024-01-17 14:31:46 -0800

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
 
+''' This module contains the function number_of_subscribers '''
+import json
 import requests
 
 
-def top_ten(subreddit):
-    """Print titles of the top ten hot posts for a subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "MyUserAgent"}
+def number_of_subscribers(subreddit):
+    ''' Gets the number of subscribers of a subreddit '''
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "GetSubscriberCount"},
+                            allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code != 200:
-            print(None)
-            return
-
-        data = response.json().get("data", {})
-        children = data.get("children", [])
-        top_posts = [(child["data"]["name"], child["data"]["title"],
-                      child["data"]["ups"]) for child in children][:10]
-
-        for post in top_posts:
-            print(post[1])
-
-    except requests.exceptions.RequestException:
-        print(None)
+    data = response.json().get('data')
+    if not data:
+        return 0
+    else:
+        return data['subscribers']
